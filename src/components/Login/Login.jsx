0c88ede9d48ff0a74d2d8fc519a767485e55e533
emoncse2020 <emon.cse.2020@gmail.com>
2025-06-03 21:38:30 +0600

    completed

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,102 @@
+import {
+  sendEmailVerification,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import auth from "../../firebase.init";
+import { useRef, useState } from "react";
+import { Link } from "react-router";
+
 const Login = () => {
+  const [success, setSuccess] = useState(false);
+  const [loginError, setLoginError] = useState("");
+  const emailRef = useRef();
+
+  const handleLogIn = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    // reset status
+
+    setSuccess(false);
+    setLoginError("");
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        // console.log(result.user);
+        if (!result.user.emailVerified) {
+          setLoginError("pleased verify your email");
+          return;
+        }
+        setSuccess(!success);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoginError("Please enter correct email or password");
+      });
+  };
+
+  const handleForgetPassword = () => {
+    // console.log("Get me an email address", emailRef.current.value);
+    const email = emailRef.current.value;
+    if (!email) {
+      alert("please provide a valid email");
+    } else {
+      sendPasswordResetEmail(auth, email).then(() => {
+        alert("Password Reset email sent");
+      });
+    }
+  };
   return (
-    <div>
-      <h2 className="text-3xl ">Login</h2>
+    <div className="hero max-w-3xl mx-auto mt-12">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+        </div>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <div className="card-body">
+            <form onSubmit={handleLogIn} className="fieldset">
+              <label className="label">Email</label>
+              <input
+                type="email"
+                ref={emailRef}
+                className="input"
+                placeholder="Email"
+                name="email"
+              />
+              <label className="label">Password</label>
+              <input
+                type="password"
+                className="input"
+                placeholder="Password"
+                name="password"
+              />
+              <div>
+                <label
+                  onClick={handleForgetPassword}
+                  className="link link-hover"
+                >
+                  Forgot password?
+                </label>
+              </div>
+              <button className="btn btn-neutral mt-4">Login</button>
+            </form>
+
+            {success && "Login Successful"}
+            {loginError && <p>{loginError}</p>}
+            <p>
+              New to this website please <Link to={"/signup"}>Sign up</Link>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,8 +1,13 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+  sendSignInLinkToEmail,
+} from "firebase/auth";
 import auth from "../../firebase.init";
 import { useState } from "react";
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
+import { Link } from "react-router";
 
 const SignUp = () => {
   const [success, setSuccess] = useState(false);
@@ -13,12 +18,14 @@ const SignUp = () => {
   const passwordRegex =
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
   const handleSignUp = (event) => {
     event.preventDefault();
     const form = event.target;
     const email = form.email.value;
     const password = form.password.value;
-    setSuccess(false);
+    const terms = form.terms.checked;
 
     if (!passwordRegex.test(password)) {
       setShowError(
@@ -26,13 +33,27 @@ const SignUp = () => {
       );
       return;
     }
+    if (!emailRegex.test(email)) {
+      setShowError("❌ Please enter a valid email address.");
+    }
 
+    if (!terms) {
+      setShowError("please accept our terms and conditions");
+      return;
+    }
+    setSuccess(false);
+    setShowError("");
     // create new user
     createUserWithEmailAndPassword(auth, email, password)
       .then((result) => {
         const user = result.user;
-        console.log(user);
+        // console.log(user);
         setSuccess(!success);
+
+        // Email verification
+        sendEmailVerification(auth.currentUser).then((result) => {
+          console.log(result.user);
+        });
       })
       .catch((error) => {
         console.log(error);
@@ -59,9 +80,24 @@ const SignUp = () => {
               placeholder="Password"
               name="password"
             />
+            <div className="relative">
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute -top-9 right-8"
+              >
+                {showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>}
+              </button>
+            </div>
+            <label className="label">
+              <input type="checkbox" className="checkbox" name="terms" />
+              <span className="label-text">Accept our terms & conditions</span>
+            </label>
 
             <div>
-              <a className="link link-hover">Forgot password?</a>
+              <p>
+                Already have an Account? <Link to={"/login"}>Log In</Link>
+              </p>
             </div>
             <button className="btn btn-neutral mt-4">Sign Up</button>
           </form>
@@ -70,14 +106,6 @@ const SignUp = () => {
           {success && (
             <p className="text-green-500">Successfully created your account</p>
           )}
-          <div className="relative">
-            <button
-              onClick={() => setShowPassword(!showPassword)}
-              className="absolute -top-30 right-8"
-            >
-              {showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>}
-            </button>
-          </div>
         </div>
       </div>
     </div>
